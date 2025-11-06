9013fc65d3afab5fd91a6367a6f589b555475a82
saurabh.virkar <saurabh.virkar1997@gmail.com>
2025-11-06 13:24:33 +0530

    added correct api

--- sk-fabricator-ui/src/app/environments/environment.prod.ts
+++ sk-fabricator-ui/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://skfabricatorapi.onrender.com' // Use your API's Render URL
+  apiUrl: 'https://skfabricatorapi.onrender.com/api' // Use your API's Render URL
 };
