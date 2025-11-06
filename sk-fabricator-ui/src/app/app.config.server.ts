45c0acefe7e3ff4fc8d5a4ad8694e73c6a7255aa
saurabh.virkar <saurabh.virkar1997@gmail.com>
2025-11-06 12:31:39 +0530

    a

--- sk-fabricator-ui/src/app/app.config.server.ts
+++ sk-fabricator-ui/src/app/app.config.server.ts
@@ -1,10 +1,20 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, ENVIRONMENT_INITIALIZER } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { APP_BASE_HREF } from '@angular/common';
+import { environment } from '../environments/environment';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    { provide: APP_BASE_HREF, useValue: '/' },
+    {
+      provide: ENVIRONMENT_INITIALIZER,
+      useValue: () => {
+        environment.apiUrl = 'https://skfabricatorapi.onrender.com'; // Use your production API URL
+      },
+      multi: true,
+    },
   ]
 };
 
