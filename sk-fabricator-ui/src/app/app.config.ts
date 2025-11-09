a66cde1e92afeb55bea5bb951e712714deed0930
saurabh.virkar <saurabh.virkar1997@gmail.com>
2025-11-09 12:41:41 +0530

    Change footer

--- sk-fabricator-ui/src/app/app.config.ts
+++ sk-fabricator-ui/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -10,7 +10,7 @@ import { AuthInterceptor } from './auth/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideClientHydration(),
     provideHttpClient(withInterceptorsFromDi(), withFetch()), // Enable HttpClient with interceptors and fetch API
     {
--- sk-fabricator-ui/src/app/footer/footer.component.ts
+++ sk-fabricator-ui/src/app/footer/footer.component.ts
@@ -3,11 +3,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router'; // Used for routerLink in template
+import { MapComponent } from '../pages/map/map.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink], 
+  imports: [CommonModule, RouterLink, MapComponent], 
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
