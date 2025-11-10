f56d16236fe6bf6b792af86e2d07abdf3a470b78
saurabh.virkar <saurabh.virkar1997@gmail.com>
2025-11-10 11:57:41 +0530

    Add correct sizw

--- sk-fabricator-ui/src/app/pages/map/map.component.ts
+++ sk-fabricator-ui/src/app/pages/map/map.component.ts
@@ -1,24 +1,19 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { InquiryFormComponent } from '../inquiry-form/inquiry-form.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-map',
+  template: `
+    <div style="width: 100%; height: 100%;">
+      <iframe
+        width="100%"
+        height="100%"
+        frameborder="0"
+        style="border:0"
+        src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Space+Needle,Seattle+WA"
+        allowfullscreen>
+      </iframe>
+    </div>
+  `,
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
-
-  private sanitizer = inject(DomSanitizer);
-  // ✅ Correct Google Maps Embed URL
-  mapEmbedString ='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.755009327827!2d73.76234127496043!3d18.60983788250992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b93ad619c783%3A0x74ec98edacfaf31e!2sS%20K%20FABRICATOR%20AND%20ERECTOR!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin';
-
-  safeMapUrl!: SafeResourceUrl;
-
-  ngOnInit(): void {
-    this.safeMapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.mapEmbedString);
-  }
-}
+export class MapComponent {}
