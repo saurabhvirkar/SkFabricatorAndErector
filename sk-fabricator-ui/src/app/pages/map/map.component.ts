98b8c21b53cdf370abb4a06e8764eea6a256df18
saurabh.virkar <saurabh.virkar1997@gmail.com>
2025-11-14 10:51:58 +0530

    Adding

--- sk-fabricator-ui/src/app/pages/map/map.component.ts
+++ sk-fabricator-ui/src/app/pages/map/map.component.ts
@@ -2,18 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-map',
-  template: `
-    <div style="width: 100%; height: 100%;">
-      <iframe
-        width="100%"
-        height="100%"
-        frameborder="0"
-        style="border:0"
-        src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Space+Needle,Seattle+WA"
-        allowfullscreen>
-      </iframe>
-    </div>
-  `,
   standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
 })
-export class MapComponent {}
+export class MapComponent {
+
+}
